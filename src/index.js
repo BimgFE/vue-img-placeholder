4a22f964331a11cb1fe00707a5b57cdb9ab9103e
xuexb <fe.xiaowu@gmail.com>
2017-05-14 22:18:17 +0800

    test npm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ImgPlaceholder from './index.vue';
 
 ImgPlaceholder.install = function (Vue) {
-    Vue.component(Img.name, ImgPlaceholder);
+    Vue.component(ImgPlaceholder.name, ImgPlaceholder);
 };
 
 export default ImgPlaceholder;
